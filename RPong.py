c8fcb26eb08a9b714e9bcda1f31a324a58f91ff5
Riedler <david.riedler@gelse.net>
2019-03-21 17:24:06 +0100

    added primitive sounds added advanced error descriptors added awesomeness

--- RPong.py
+++ RPong.py
@@ -52,9 +52,9 @@ class Ball(rgr.graphic):
 			self.posx-=1
 			self.posy-=1
 	def bouncewalls(self):
-		if self.posx>19:
+		if self.posx>19 and self.dr in [0,1]:
 			return ("win",True)
-		elif self.posx<0:
+		elif self.posx<0 and self.dr in [2,3]:
 			return ("win",False)
 		if self.posy>13:
 			if self.dr==1:
@@ -71,7 +71,7 @@ class Ball(rgr.graphic):
 		if self.posx<=1 or self.posx>=18:
 			for pad in pads:
 				if self.posy-pad.posy in range(len(pad.content)):
-					self.content=[["\033[35m"+shds.a+"\033[0m"]]
+					self.content=[["\a\033[35m"+shds.a+"\033[0m"]]
 					if self.dr==2 and pad.fp:
 						self.dr=1
 					elif self.dr==3 and pad.fp:
@@ -80,21 +80,18 @@ class Ball(rgr.graphic):
 						self.dr=3
 					elif self.dr==1 and not pad.fp:
 						self.dr=2
-					else:
-						if self.posx<=1:
-							self.posx=2
-						elif self.posx>=19:
-							self.posx=18
+					elif self.dr not in [0,1,2,3]:
+						raise ValueError("\aDirection Variable got an impossible Value! ("+str(self.dr)+")")
 					touching=True
 		if not touching:
 			self.content=[["\033[31m"+shds.a+"\033[0m"]]
 	def checkerrs(self):
 		if not self.dr in [0,1,2,3]:
-			raise ValueError("Direction Variable got an impossible Value!")
+			raise ValueError("\aDirection Variable got an impossible Value!")
 		if self.posx<0:
-			raise ValueError("Negative XPosition Value is invalid.")
+			raise ValueError("\aNegative XPosition Value is invalid.")
 		if self.posy<0:
-			raise ValueError("Negative YPosition Value is invalid.")
+			raise ValueError("\aNegative YPosition Value is invalid.")
 class NewGame(Exception):
 	pass
 class EndGame(Exception):
@@ -143,35 +140,39 @@ def main(stdscr):
 		screen.display()
 		print("\033[16;0H"+str(score[0])+"\033[16;14HPress ENTER to Start\033[16;40H"+str(score[1]))
 	print("\033[16;14H                    ")
-	while True:
-		for x in range(100):
-			try:
-				key=(stdscr.getkey())
-			except curses.error:
-				key=None
-			globkeyfunc(key)
-			pada.keyfunc(key)
-			padb.keyfunc(key)
-		ball.checkerrs()
-		ball.bouncepads((pada,padb))
-		status=ball.bouncewalls()
-		ball.move()
+#Here starts the true Main Loop				#
+	while True:								#
+		for x in range(100):				#
+			try:							#
+				key=(stdscr.getkey())		#
+			except curses.error:			#
+				key=None					#
+			globkeyfunc(key)				#
+			pada.keyfunc(key)				#
+			padb.keyfunc(key)				#
+		ball.checkerrs()					#
+		ball.bouncepads((pada,padb))			#
+		ball.move()							#
+		status=ball.bouncewalls()			#
 		if type(status)==tuple and status[0]=="win":
-			win(fp=status[1])
-			del ball
-			ball=Ball()
-			spdlimiter.spd=5
-		screen.draw(pada)
-		screen.draw(padb)
-		screen.draw(ball)
-		screen.display()
-		spdlimiter.sleep()
+			win(fp=status[1])				#
+			del ball						#
+			ball=Ball()						#
+			spdlimiter.spd=5				#
+		screen.draw(pada)					#
+		screen.draw(padb)					#	
+		screen.draw(ball)					#
+		screen.display()					#
+		spdlimiter.sleep()					#
 		spddisp=str(round(spdlimiter.spd*100))
-		if spdlimiter.slow:
+		if spdlimiter.slow:					#
 			spddisp="\033[31m"+spddisp+"\033[0m"
 		print("\033[16;0H"+str(score[0])+"\033[16;14HSpeed:"+spddisp+"\033[16;"+str(41-len(str(score[1])))+"H"+str(score[1]))
-		spdlimiter.start()
+		spdlimiter.start()					#
+#And so it beg...
+#ends.
 running=True
+print("\033[?25l")
 while running==True:
 	running=False
 	try:
@@ -180,7 +181,7 @@ while running==True:
 		print("\033[2J")
 		running=True
 	except EndGame:
-		print("\033[2J")
+		print("\033[2J\033[?25h")
 		running=False
 	except KeyboardInterrupt:
-		raise Warning("Don't do this. It can mess with your console. Press q instead.")
+		raise Warning("\aDon't do this. It can mess with your console. Press q instead.")
